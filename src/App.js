df66288b8c1bba7997316f2fc9216538cc5337b8
dcamriff <driffelatl@gmail.com>
2018-01-18 19:58:04 -0500

    installed axios and added CreditsPage, CreditsList and Credit components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React, {Component} from 'react'
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom'
 import Home from './components/Home'
 import UserProfile from './components/UserProfile'
 import LogIn from './components/LogIn'
+import CreditsPage from './components/CreditsPage'
+
+import axios from 'axios'
 
 class App extends Component {
 
@@ -11,7 +14,17 @@ class App extends Component {
       currentUser: {
         userName: 'bob_loblaw',
         memberSince: '08/23/99'
-      }
+      },
+      credits: [],
+      debits: []
+    }
+
+    getCredits = () => {
+      axios.get('http://localhost:4000/credits')
+      .then((response) => {
+        const credits = response.data
+        this.setState({credits})
+      })
     }
 
     mockLogIn = (logInInfo) => {
@@ -20,27 +33,38 @@ class App extends Component {
       this.setState({currentUser: newUser})
     }
 
+    componentWillMount () {
+      this.getCredits()
+    }
+
   render() {
+    const accountBalance = this.calculateAccountBalance
     const HomeComponent = () => (<Home accountBalance={this.state.accountBalance}/>)
     
     const UserProfileComponent = () => (<UserProfile
       userName={this.state.currentUser.userName}
       memberSince={this.state.currentUser.memberSince}/>)
 
-    const LogInComponent = () => (
-    <LogIn
-      user={this.state.currentUser}
-      mockLogIn={this.mockLogIn}
-      {...this.props}/>
-      )
+    const LogInComponent = () => (<LogIn user={this.state.currentUser} mockLogIn={this.mockLogIn} {...this.props}/>)
+
+    const CreditsPageComponent = () => (<CreditsPage credits={this.state.credits} accountBalance={accountBalance} {...this.props} />)
+
 
     return (
       <Router>
+        <div>
+          <div>
+            <Link to="/">Home</Link><br/>
+            <Link to="/login">Log in</Link>
+          </div>
+        
         <Switch>
           <Route exact path="/" render={HomeComponent}/>
           <Route exact path="/userProfile" render={UserProfileComponent}/>
           <Route exact path="/login" render={LogInComponent}/>
+          <Route exact path="/credits" render={CreditsPageComponent}/>
         </Switch>
+        </div>
       </Router>
     )
   }
