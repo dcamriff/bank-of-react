cc6f78bcc4cdde3f73552570ca9eca6253f2d9c2
dcamriff <driffelatl@gmail.com>
2018-01-20 16:25:06 -0500

    Added Debit component

---
+++ src/components/Debit.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+const Debit = (props) => {
+    return (
+        <div>
+            <span>Descrition: {props.description}</span>
+            <span> | </span>
+            <span>Amount: {props.amount}</span>
+            <span>Date: props.date}</span>
+        </div>
+    )
+}
+
+export default Debit
